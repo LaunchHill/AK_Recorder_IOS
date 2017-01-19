98df8b4ac78a065a6b8226842ba87f10a080e6fe
DEV_IOS <332004729@qq.com>
2017-01-19 14:10:52 +0800

    Initial Commit

---
+++ Kitchen_DDMTests/Kitchen_DDMTests.m
@@ -0,0 +1,39 @@
+//
+//  Kitchen_DDMTests.m
+//  Kitchen_DDMTests
+//
+//  Created by 郁兵生 on 2017/1/19.
+//  Copyright © 2017年 郁兵生. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Kitchen_DDMTests : XCTestCase
+
+@end
+
+@implementation Kitchen_DDMTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
